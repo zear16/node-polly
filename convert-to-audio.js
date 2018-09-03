d29675b79a3b25007f4a6529826ca371451425c9
Zear16 <zear16@gmail.com>
2018-09-03 14:09:00 +0700

    Fixed: Split text

--- convert-to-audio.js
+++ convert-to-audio.js
@@ -36,96 +36,97 @@ module.exports.handler = function (event, context, callback) {
             // Because single invocation of the polly synthesize_speech api can
             // transform text with about 1,500 characters, we are dividing the
             // post into blocks of approximately 1,000 characters.
-            //var textBlocks = text.match(/.{1,1000}/g);
 
             console.log(`text: ${text}, voice: ${voice}`);
 
-            // TODO : Convert text to audio only 1000 character
-            polly.synthesizeSpeech({
-                Text: text,
-                VoiceId: voice,
-                OutputFormat: 'mp3'
-            }, function(err, audio) {
-                if (err) {
-                    callback(err);
-                } else {
-                    // Save the audio stream returned by Amazon Polly on Lambda's temp
-                    // directory. If there are multiple text blocks, the audio stream
-                    // will be combined into a single file.
-                    var tempFile = path.join('/tmp', postId);
-                    if (audio.AudioStream instanceof Buffer) {
-                        console.log(`tempFile: ${tempFile}`);
-                        fs.writeFileSync(tempFile, audio.AudioStream);
-                        fs.readFile(tempFile, null, function (err, audioData) {
-                            if (err) {
-                                callback(err);
-                            } else {
-                                var uploadParams = {
-                                    Bucket: process.env.BUCKET_NAME,
-                                    Body: audioData,
-                                    Key: postId + '.mp3'
-                                };
-                                s3.putObject(uploadParams, function(err, data) {
-                                    if (err) {
-                                        callback(err);
-                                    } else {
-                                        console.log('data', JSON.stringify(data, null, 2));
-                                        s3.putObjectAcl({
-                                            Bucket: process.env.BUCKET_NAME,
-                                            ACL: 'public-read',
-                                            Key: postId + '.mp3'
-                                        }, function(err, data) {
-                                            if (err) {
-                                                callback(err);
-                                            } else {
-                                                s3.getBucketLocation({
-                                                    Bucket: process.env.BUCKET_NAME
-                                                }, function(err, location) {
-                                                    if (err) {
-                                                        callback(err);
-                                                    } else {
-                                                        console.log('location', JSON.stringify(location, null, 2));
-                                                        var region = location.LocationConstraint;
-                                                        console.log(`region: ${region}`);
-                                                        var urlBegining;
-                                                        if (region) {
-                                                            urlBegining = `https://s3-${region}.amazonaws.com`;
-                                                        } else {
-                                                            urlBegining = 'https://s3.amazonaws.com';
-                                                        }
-                                                        var url = `${urlBegining}/${process.env.BUCKET_NAME}/${postId}.mp3`;
-                                                        console.log(`url: ${url}`);
-                                                        docClient.update({
-                                                            TableName: process.env.DB_TABLE_NAME,
-                                                            Key: {
-                                                              'id': postId
-                                                            },
-                                                            UpdateExpression: 'SET #statusAtt = :statusValue, #urlAtt = :urlValue',
-                                                            ExpressionAttributeValues: {
-                                                                ':statusValue': 'UPDATED', ':urlValue': url
-                                                            },
-                                                            ExpressionAttributeNames: {
-                                                                '#statusAtt': 'status', '#urlAtt': 'url'
-                                                            }
-                                                        }, function(err, res) {
-                                                            if (err) {
-                                                                callback(err);
-                                                            } else {
-                                                                callback(null, res);
-                                                            }
-                                                        });
-                                                    }
-                                                });
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
+            var textBlocks = text.match(/.{1,1000}/g);
+            textBlocks.forEach(block => {
+                polly.synthesizeSpeech({
+                    Text: text,
+                    VoiceId: voice,
+                    OutputFormat: 'mp3'
+                }, function(err, audio) {
+                    if (err) {
+                        callback(err);
                     } else {
-                        callback('AudioStream is not buffer');
+                        // Save the audio stream returned by Amazon Polly on Lambda's temp
+                        // directory. If there are multiple text blocks, the audio stream
+                        // will be combined into a single file.
+                        var tempFile = path.join('/tmp', postId);
+                        if (audio.AudioStream instanceof Buffer) {
+                            console.log(`tempFile: ${tempFile}`);
+                            fs.writeFileSync(tempFile, audio.AudioStream);
+                            fs.readFile(tempFile, null, function (err, audioData) {
+                                if (err) {
+                                    callback(err);
+                                } else {
+                                    var uploadParams = {
+                                        Bucket: process.env.BUCKET_NAME,
+                                        Body: audioData,
+                                        Key: postId + '.mp3'
+                                    };
+                                    s3.putObject(uploadParams, function(err, data) {
+                                        if (err) {
+                                            callback(err);
+                                        } else {
+                                            console.log('data', JSON.stringify(data, null, 2));
+                                            s3.putObjectAcl({
+                                                Bucket: process.env.BUCKET_NAME,
+                                                ACL: 'public-read',
+                                                Key: postId + '.mp3'
+                                            }, function(err, data) {
+                                                if (err) {
+                                                    callback(err);
+                                                } else {
+                                                    s3.getBucketLocation({
+                                                        Bucket: process.env.BUCKET_NAME
+                                                    }, function(err, location) {
+                                                        if (err) {
+                                                            callback(err);
+                                                        } else {
+                                                            console.log('location', JSON.stringify(location, null, 2));
+                                                            var region = location.LocationConstraint;
+                                                            console.log(`region: ${region}`);
+                                                            var urlBegining;
+                                                            if (region) {
+                                                                urlBegining = `https://s3-${region}.amazonaws.com`;
+                                                            } else {
+                                                                urlBegining = 'https://s3.amazonaws.com';
+                                                            }
+                                                            var url = `${urlBegining}/${process.env.BUCKET_NAME}/${postId}.mp3`;
+                                                            console.log(`url: ${url}`);
+                                                            docClient.update({
+                                                                TableName: process.env.DB_TABLE_NAME,
+                                                                Key: {
+                                                                  'id': postId
+                                                                },
+                                                                UpdateExpression: 'SET #statusAtt = :statusValue, #urlAtt = :urlValue',
+                                                                ExpressionAttributeValues: {
+                                                                    ':statusValue': 'UPDATED', ':urlValue': url
+                                                                },
+                                                                ExpressionAttributeNames: {
+                                                                    '#statusAtt': 'status', '#urlAtt': 'url'
+                                                                }
+                                                            }, function(err, res) {
+                                                                if (err) {
+                                                                    callback(err);
+                                                                } else {
+                                                                    callback(null, res);
+                                                                }
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            callback('AudioStream is not buffer');
+                        }
                     }
-                }
+                });
             });
         }
     });
